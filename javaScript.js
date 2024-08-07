3c047b84fea75d5dc23f17d150bb6ad3407c8e41
2008oxfordcomma <2008oxfordcomma@gmail.com>
2024-08-07 15:46:08 -0500

    Added cool animations

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,2 @@
-const blurDivs = document.querySelectorAll(".photo-item");
 
-      blurDivs.forEach((div) => {
-        const img = div.querySelector("img");
 
-        function loaded() {
-          div.classList.add("loaded");
-        }
-
-        if (img.complete) {
-          loaded();
-        } else {
-          img.addEventListener("load", loaded);
-        }
-      });
